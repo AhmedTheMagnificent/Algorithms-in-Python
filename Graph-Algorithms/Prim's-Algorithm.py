27e4c3cf2af94d98845b33f0fec8286ca846e8fb
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-11-27 23:54:37 +0500

    added prims algorithm

--- Graph-Algorithms/Prim's-Algorithm.py
+++ Graph-Algorithms/Prim's-Algorithm.py
@@ -2,30 +2,40 @@ import heapq
 import random
 
 class Graph():
-    def __init__(self):
-        self.graph = {}
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = {i : [] for i in range(self.vertices)}
         self.MST = {}
         
     def addEdge(self, u, v, w):
-        if u not in self.graph:
-            self.graph[u] = {}
-        if v not in self.graph:
-            self.graph[v] = {}
-            
-        self.graph[u][v] = w
-        self.graph[v][w] = w
+        self.graph[u].append((w, v))
+        self.graph[v].append((w, u))
         
     def primMST(self):
-        anynode = random.randrange(0, len(self.graph) - 1)
-        visited = {}
+        cost = 0
+        visited = set()
         heap = []
+        visited.add(0)
+        for edge in self.graph[0]:
+            heapq.heappush(heap, edge)
+        
+        while len(visited) < self.vertices:
+            weight, node = heapq.heappop(heap)
+            if node in visited:
+                continue
+            cost += weight
+            visited.add(node)
+            for edge in self.graph[node]:
+                if edge[1] not in visited:
+                    heapq.heappush(heap, edge)
+        return cost
         
         
-        
-g = Graph()
+g = Graph(4)
 g.addEdge(0, 1, 2)
 g.addEdge(0, 3, 6)
 g.addEdge(1, 3, 3)
 g.addEdge(2, 1, 1)
 g.addEdge(2, 3, 4)
-g.primMST()
+cost = g.primMST()
+print(cost)
